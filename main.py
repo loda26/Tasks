5b29ab29741c74225e03dc5734a84de4c4ef6d81
loda26 <mokhaled26498@gmail.com>
2024-05-05 01:41:40 +0300

    loda

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from models.users import Users, Session
 
 app = Flask(__name__)
 
-Session = Session()
+session = Session()
 
 @app.route('/loda')
 def index():
-    users = Session.query(Users).all()
+    users = session.query(Users).all()
     return render_template('index.html', users=users)
 
 @app.route('/loda/submit', methods=['POST'])
@@ -17,17 +17,17 @@ def submit():
     password = request.form['password']
 
     user = Users(name=name, email=email, password=password)
-    Session.add(user)
-    Session.commit()
+    session.add(user)
+    session.commit()
 
     return redirect('/loda')
 
 @app.route('/loda/delete/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    user = Session.query(Users).filter_by(id=user_id).first()
+    user = session.query(Users).filter_by(id=user_id).first()
     if user:
-        Session.delete(user)
-        Session.commit()
+        session.delete(user)
+        session.commit()
         return "User deleted successfully", 200
     else:
         return "User not found", 404
